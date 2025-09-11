d162231dec0c894d839baec640401d142ca1b871
Ibrahim-Oughfayn <oughfayn.mt@gmail.com>
2025-09-11 12:07:09 +0100

    changes

--- functions.h
+++ functions.h
@@ -2,6 +2,8 @@
 #include <string.h>
 #define size_avions 100
 #define size_aeroport 50
+int id = 0;
+int nbrAvions = 0;
 
 // date structur
 typedef struct
@@ -31,8 +33,8 @@ typedef struct
 void addAeroport(aeroport arports[size_aeroport])
 {
     char nom[50];
-    printf("donner moi le nom de l'aeroport:");
-    scanf(" %[^\n] ", nom);
+    printf("donner moi le nom de l'eroport:");
+    scanf(" %[^\n]", nom);
     for (int i = 0; i < size_aeroport; i++)
     {
         if (strcmp(arports[i].nom, nom) == 0)
@@ -65,6 +67,144 @@ void listAeroports(aeroport arports[size_aeroport])
         else
         {
             printf(" --> l'areport %s contient %d avions\n", arports[i].nom, arports[i].nbrAvions);
+        }
+    }
+}
+
+// get index airoport by nom
+int getibynom(aeroport arports[size_aeroport], char nom[50])
+{
+    for (int i = 0; i <= 0; i++)
+    {
+        if (strcasecmp(arports[i].nom, nom) == 0)
+        {
+            return i;
+        }
+    }
+}
+
+// afficher tout les avions
+void list_avions(aeroport arports[size_aeroport], char nom[50])
+{
+    int a = getibynom(arports, nom);
+    printf("%d", a);
+    for (int i = 0; i <= nbrAvions; i++)
+    {
+        if (!arports[a].avions[i].idAvion)
+        {
+            return;
+        }
+        printf("%d- id: %d,model: %s,capacite: %d,status: %s,date entre: %d/%d/%d \n", i + 1, arports[a].avions[i].idAvion, arports[a].avions[i].model, arports[a].avions[i].capacite, arports[a].avions[i].status, arports[a].avions[i].dtEntree.d, arports[a].avions[i].dtEntree.m, arports[a].avions[i].dtEntree.y);
+    }
+}
+
+// ajouter avion
+void add_avion(aeroport arports[size_aeroport], char nom[50])
+{
+    id++;
+    char model[20], status[20];
+    int cap, d, m, y;
+    printf("\ndonner moi le model de l'avion:");
+    scanf(" %[^\n]", model);
+    printf("donner moi le capacite de l'avion:");
+    scanf(" %d", &cap);
+    printf("donner moi le status de l'avion:");
+    scanf(" %[^\n]", status);
+    printf("donner moi la date d'entre de l'avion 'dd/mm/yyyy':");
+    scanf("%d/%d/%d", &d, &m, &y);
+    int a = getibynom(arports, nom);
+    arports[a].avions[nbrAvions].idAvion = id;
+    strcpy(arports[a].avions[nbrAvions].model, model);
+    strcpy(arports[a].avions[nbrAvions].status, status);
+    arports[a].avions[nbrAvions].capacite = cap;
+    arports[a].avions[nbrAvions].dtEntree.d = d;
+    arports[a].avions[nbrAvions].dtEntree.m = m;
+    arports[a].avions[nbrAvions].dtEntree.y = y;
+    nbrAvions++;
+}
+
+// modifier un avion modele capacite statut
+void updateAvion(aeroport arports[size_aeroport], char nom[50])
+{
+    int idToUpdate;
+    printf("donner moi l'id de l'avion qui vouv vouler modifier:");
+    scanf("%d", &idToUpdate);
+    int a = getibynom(arports, nom);
+    for (int i = 0; i <= nbrAvions; i++)
+    {
+        if (arports[a].avions[i].idAvion == idToUpdate)
+        {
+            char mod[20], statut[20];
+            int cap;
+            printf("donner moi la nouvel modele: ");
+            scanf(" %[^\n]", mod);
+            printf("donner moi la nouvel statut: ");
+            scanf(" %[^\n]", statut);
+            printf("donner moi la nouvel capacite: ");
+            scanf("%d", &cap);
+            strcpy(arports[a].avions[i].model, mod);
+            strcpy(arports[a].avions[i].status, statut);
+            arports[a].avions[i].capacite = cap;
+            printf("the avion updated succesfully!!\n");
+            return;
+        }
+    }
+}
+
+// suppremer un avion
+void deletAvion(aeroport arports[size_aeroport], char nom[50])
+{
+    int idTodelete;
+    printf("donner moi l'id de l'avion qui vous vouler supprimer:");
+    scanf("%d", &idTodelete);
+    int a = getibynom(arports, nom);
+    for (int i = 0; i <= nbrAvions; i++)
+    {
+        if (arports[a].avions[i].idAvion == idTodelete)
+        {
+            arports[a].avions[i].idAvion = arports[a].avions[i + 1].idAvion;
+            strcpy(arports[a].avions[i].model, arports[a].avions[i + 1].model);
+            strcpy(arports[a].avions[i].status, arports[a].avions[i + 1].status);
+            arports[a].avions[i].capacite = arports[a].avions[i + 1].capacite;
+            arports[a].avions[i].dtEntree.d = arports[a].avions[i + 1].dtEntree.d;
+            arports[a].avions[i].dtEntree.m = arports[a].avions[i + 1].dtEntree.m;
+            arports[a].avions[i].dtEntree.y = arports[a].avions[i + 1].dtEntree.y;
+            printf("the avion deleted succesfully\n");
+            nbrAvions--;
+        }
+    }
+}
+
+// search avion by id
+
+void searachById(aeroport arports[size_aeroport], char nom[50])
+{
+    int idToResearch;
+    int a = getibynom(arports, nom);
+    printf("\nentrer l'id de l'avion qui vous rechercher:");
+    scanf("%d", &idToResearch);
+    for (int i = 0; i <= nbrAvions; i++)
+    {
+        if (arports[a].avions[i].idAvion == idToResearch)
+        {
+            printf("this is the avion that you looking for:\n");
+            printf("%d- id: %d,model: %s,capacite: %d,status: %s,date entre: %d/%d/%d \n", i + 1, arports[a].avions[i].idAvion, arports[a].avions[i].model, arports[a].avions[i].capacite, arports[a].avions[i].status, arports[a].avions[i].dtEntree.d, arports[a].avions[i].dtEntree.m, arports[a].avions[i].dtEntree.y);
+        }
+    }
+}
+
+void searachByModel(aeroport arports[size_aeroport], char nom[50])
+{
+    char model[20];
+    int a = getibynom(arports, nom);
+    printf("\nentrer le model de l'avion qui vous rechercher:");
+    scanf(" %[^\n]", model);
+    for (int i = 0; i <= nbrAvions; i++)
+    {
+        if (strcasecmp(arports[a].avions[i].idAvion,model)==0)
+        {
+            printf("voila les avion de model %s:\n",&model);
+            printf("%d- id: %d,model: %s,capacite: %d,status: %s,date entre: %d/%d/%d \n", i + 1, arports[a].avions[i].idAvion, arports[a].avions[i].model, arports[a].avions[i].capacite, arports[a].avions[i].status, arports[a].avions[i].dtEntree.d, arports[a].avions[i].dtEntree.m, arports[a].avions[i].dtEntree.y);
         }
     }
 }
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "functions.h"
 
+void avionsMenu(aeroport arpts[size_aeroport], char nom[50]);
 
 void aeroportsMenu(aeroport arpts[size_aeroport])
 {
@@ -16,7 +17,6 @@ void aeroportsMenu(aeroport arpts[size_aeroport])
         printf("#    4-quitter le program                                                                                         #\n");
         printf("#=================================================================================================================#\n");
         printf("#taper votre choix ici:");
-
         int ch;
         scanf("%d", &ch);
         switch (ch)
@@ -33,8 +33,8 @@ void aeroportsMenu(aeroport arpts[size_aeroport])
         {
             char name[50];
             printf("donner moi le nom de l'eroport :");
-            scanf("%[^\n]", name);
-            avionsMenu(name);
+            scanf(" %[^\n]", name);
+            avionsMenu(arpts, name);
             break;
         }
         case 4:
@@ -45,18 +45,24 @@ void aeroportsMenu(aeroport arpts[size_aeroport])
     }
 }
 
-void avionsMenu(char nom[50])
+void avionsMenu(aeroport arpts[size_aeroport], char nom[50])
 {
     while (1)
     {
         printf("#=================================================================================================================#\n");
-        printf("##########################################  la gestion de l'eroport %s  ###########################################\n",nom);
+        printf("########################################  la gestion de l'eroport %s  #########################################\n", nom);
         printf("#=================================================================================================================#\n");
         printf("#  choser un choix:                                                                                               #\n");
-        printf("#    1-afficher tout les aeroports                                                                                #\n");
-        printf("#    2-ajouter un aeroport                                                                                        #\n");
-        printf("#    3-gerer un aeroport                                                                                          #\n");
-        printf("#    4-quitter le program                                                                                         #\n");
+        printf("#    1-afficher tout les avions                                                                                   #\n");
+        printf("#    2-ajouter un avion                                                                                           #\n");
+        printf("#    3-Modifier un avion                                                                                          #\n");
+        printf("#    4-Supprimer un avion.                                                                                        #\n");
+        printf("#    7-Trier les avions par la capacite                                                                           #\n");
+        printf("#    8-Trier les avions par modele alphabetique                                                                   #\n");
+        printf("#    5-Rechercher un avion par id                                                                                 #\n");
+        printf("#    6-Rechercher un avion par modele                                                                             #\n");
+        printf("#    9-retourn a l'menu des eroports                                                                              #\n");
+        printf("#    0-quitter le program                                                                                         #\n");
         printf("#=================================================================================================================#\n");
         printf("#taper votre choix ici:");
         int ch;
@@ -65,16 +71,41 @@ void avionsMenu(char nom[50])
         {
         case 1:
         {
+            list_avions(arpts, nom);
+            continue;
         }
         case 2:
         {
+            add_avion(arpts, nom);
+            continue;
         }
         case 3:
         {
+            updateAvion(arpts, nom);
+            continue;
         }
         case 4:
         {
+            deletAvion(arpts, nom);
+            continue;
+        }
+        case 5:
+        {
+            searachById(arpts, nom);
+            continue;
+        }
+         case 5:
+        {
+            searsearachByModelachById(arpts, nom);
+            continue;
+        }
+        case 0:
+        {
             return;
+        }
+        case 9:
+        {
+            aeroportsMenu(arpts);
         }
         default:
         {
@@ -88,6 +119,9 @@ void avionsMenu(char nom[50])
 int main()
 {
     aeroport arpts[size_aeroport];
+    
     aeroportsMenu(arpts);
+
+
     return 0;
 }
